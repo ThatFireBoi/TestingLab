108bd7d21c53804ba892c4051f6592513c2e61a9
ThatFireBoi <gcf2007@hotmail.com>
2024-12-10 19:54:00 -0400

    Hashmap update

--- app/src/main/java/com/example/testinglab/Database.kt
+++ app/src/main/java/com/example/testinglab/Database.kt
@@ -1,15 +1,15 @@
 package com.example.testinglab
 
 object Database {
-    private val users = mutableListOf<User>()
+    private val users = hashMapOf<String, User>()
 
     // Saves a user to the database
     fun saveUser(user: User) {
-        users.add(user)
+        users[user.email] = user
     }
 
     // Retrieves a user by email from the database
     fun getUserByEmail(email: String): User? {
-        return users.find { it.email == email }
+        return users[email]
     }
 }
